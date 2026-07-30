78b362a58e305b187b3449ff80f3153e87b25a05
Pranav09-AI <keesarasaipranav.9@gmail.com>
2026-07-30 18:57:10 +0530

    Added changes

---
+++ backend/app/services/gemini_service.py
@@ -0,0 +1,18 @@
+from google import genai 
+from app.config import settings 
+
+class GeminiService:
+    def __init__(self):
+        self.client = genai.Client(
+            api_key=settings.GEMINI_API_KEY
+        )
+
+    def generate_response(self, prompt: str) -> str:
+        response = self.client.interactions.create(
+            model = "gemini-3.6-flash",
+            contents = prompt,
+        )
+
+        return response.output_text
+
+gemini_service = GeminiService()
