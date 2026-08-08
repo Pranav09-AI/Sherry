05e27d2d3b4d2a101bf53a041cfc04ed139d9086
Pranav09-AI <keesarasaipranav.9@gmail.com>
2026-08-08 20:27:03 +0530

    Added pipe that connect rag

--- backend/app/services/ingestion_service.py
+++ backend/app/services/ingestion_service.py
@@ -1,4 +1,27 @@
-from services.document_loader import load_path, load_pdf
+import uuid 
+import os
+
+
+from services.document_loader import load_path
 from services.text_splitter import split_documents
 from services.vector_store import create_vector_store
+from services.embedding import embeddings
+
+def ingest_document(file_path):
+    documents = load_path(file_path)
+
+    chunks = split_documents(documents)
+
+    document_id = str(uuid.uuid4())
+
+    source = os.path.basename(file_path)
+
+    vector_store = create_vector_store(
+        chunks,
+        document_id,
+        source,
+        embeddings
+    )
+
+    return vector_store
 
--- backend/app/services/text_splitter.py
+++ backend/app/services/text_splitter.py
@@ -1,14 +1,12 @@
-from app.services.document_loader import load_path,load_pdf
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-documents = load_pdf(load_path)
 
 def split_documents(documents):
 
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 100,
-        chunk_overlap = 10,
-        length_function = len,
+        chunk_size=100,
+        chunk_overlap=10,
+        length_function=len,
         is_separator_regex=False,
     )
 
