defe40b5824eb420e59e9aa44554a74c1c443521
Pranav09-AI <keesarasaipranav.9@gmail.com>
2026-08-29 21:33:01 +0530

    Fixed retiveral part

--- backend/app/services/retrevial.py
+++ backend/app/services/retrevial.py
@@ -1,11 +1,7 @@
-from langchain_chroma import Chroma
+from app.services.vector_store import vector_store
 from app.services.embedding import embeddings
 
-vector_store = Chroma(
-    collection_name="sherry_documents",
-    embedding_function=embeddings,
-    persist_directory="./chroma_db",
-)
+
 
 def retrieve(query: str)-> str:
     result = vector_store.similarity_search (
